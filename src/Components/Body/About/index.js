7d7c41a3287427cd3d12cdbf3965205aecc6de28
Anushka Dubey <71165722+anushkadubey@users.noreply.github.com>
2023-07-22 19:40:22 +0530

    Update index.js

--- src/Components/Body/About/index.js
+++ src/Components/Body/About/index.js
@@ -6,10 +6,9 @@ function About() {
         <div className="about">
             <label className="sectionTitle">About me</label>
             <div className="aboutInfo">
-            <p>Hi! I'm <span className="impName">Anushka</span>, a final year undergraduate at <span className="impName">NIT Jamshedpur</span>. I'm pursuing my Bachelors in <span className="impName">Electronics and Communication Engineering</span>.
-                I love developing and designing web apps. Currently, I'm also working upon my knowledge of Data Structures and Algorithms.</p>
-            <p>My aspirations include learning more about web development which will help me create strong and useful web applications. I also want to contribute to the community by working on diverse open source projects.</p>
-            <p>A constant learner who believes in mutual learning and growth.</p>
+            <p>Hey there! I'm <span className="impName">Anushka</span>, a web dev enthusiast who is always fascinated by the ever-evolving technology and the ground-breaking innovations. I am a fresh grad, currently working as an <span className="impName">Associate Software Engineer</span> at <span className="impName">Lowe's</span>.
+            <p>My aspirations include learning more about web development and other domains which will help me create strong and useful web applications.</p>
+            <p>I constantly try to contribute more to several open source projects to give back to the community and enhance my skill set.</p>
             </div>
             <div className="aboutPicture">
                 <img src={require("../../../Assets/Codinganime.png")} alt="" className="aboutPictureLogo" />
